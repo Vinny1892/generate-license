4b9ed3b5a43fff087f4221136534d96f1270ac27
Vinicius Espindola <vinnyaoe@gmail.com>
2021-07-24 13:18:52 -0400

    basic generate license working

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,58 @@
+use std::io::{Error,Write};
+use tera::{Tera,Context};
+use std::fs::File;
 use rust_embed::RustEmbed;
+
 #[derive(RustEmbed)]
 #[folder = "src/templates"]
 struct Asset;
 
 fn main() {
-        // let seila = Asset::get("teste.j2").map_or_else(
-        //     ||{
-        //         println!("{}", "File not found");
-        //         std::process::exit(1);
-        //     },
-        //     |d| d
-        // );
-        let testee = match Asset::get("teste.j2") {
+        let template_name = "license_template";
+        let license_template_binary = match Asset::get("teste.j2") {
             Some(content) => content,
             None =>   {
             println!("file not found");
             std::process::exit(1);
         } 
-         };
+        };
         
-        let teste =  String::from_utf8_lossy(&testee);
-        println!("{}", teste);
-        for file in Asset::iter() {
-            println!("{}", file.as_ref());
+        let license_template =  String::from_utf8_lossy(&license_template_binary);
+        // for file in Asset::iter() {
+        //     println!("{}", file.as_ref());
+        // }
+    let mut tera = Tera::default();
+    let mut context = Context::new();
+    context.insert("author", "Vinicius Espindola");
+    context.insert("year", "2019");
+    tera.add_raw_template("license_template", license_template.as_ref()).unwrap();
+    let mut context = Context::new();
+    context.insert("author", "Vinicius Espindola");
+    context.insert("year", "2021");
+    let license =  match tera.render(template_name, &context){
+        Ok(t) => t,
+        Err(e) => {
+            println!("Error {}" , e);
+            std::process::exit(1);
         }
-//     let tera = match Tera::new("src/templates/*.j2") {
-//         Ok(t) => t,
-//         Err(e) => {
-//             println!("Parsing error(s): {}", e);
-//             ::std::process::exit(1);
-//         }
-//     };
-//     let mut context = Context::new();
-//     context.insert("author", "Vinicius Espindola");
-//     context.insert("year", "2019");
-//    let result = match tera.render("teste.j2", &context){
-//         Ok(t) => t,
-//         Err(why) => panic!("{}", why)
-//    };
-//    create_file_license(result);
+    };
+    match create_file_license(license) {
+    Ok(_t) =>  { 
+        println!("File created successfully");
+        std::process::exit(0);
+    },
+    Err(_e) => {
+        println!("Error to create file license");
+        std::process::exit(1);
+    }
+   };
 }
 
 
-// fn create_file_license( content: String) {
-//     let mut file =  match File::create("license"){
-//         Err(why) => panic!("couldn't create  {}",  why),
-//         Ok(file) => file,
-//     };
-//     let _result = file.write_all(content.as_bytes());
-// }
+fn create_file_license( content: String) -> Result<(),Error> {
+    let mut file =  match File::create("license"){
+        Err(why) => panic!("couldn't create  {}",  why),
+        Ok(file) => file,
+    };
+    file.write_all(content.as_bytes())
+}
